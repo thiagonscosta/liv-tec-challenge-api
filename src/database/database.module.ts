a6b4c79cfc39771b841220e681acaecf1f7d3a18
ufc186.costa <ufc186.costa@lgepartner.com>
2021-06-25 18:26:15 +0100

    deploy configuration

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -3,16 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { List } from 'src/list/entities/list.entity';
 import { Task } from 'src/task/entities/task.entity';
 import 'dotenv/config';
-
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: process.env.DB_HOST,
       port: Number(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
+      username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      database: process.env.DATABASE,
       entities: [List, Task],
       synchronize: true,
     }),
